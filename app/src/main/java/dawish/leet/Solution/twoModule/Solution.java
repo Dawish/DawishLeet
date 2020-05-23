a58439f95470ee9f4a514d354dd2ab96f0cc8207
dawish <742637254@qq.com>
2020-05-23 16:25:29 +0800

    检查二叉树平衡性

--- app/src/main/java/dawish/leet/Solution/twoModule/Solution.java
+++ app/src/main/java/dawish/leet/Solution/twoModule/Solution.java
@@ -24,4 +24,29 @@ public class Solution {
         return Math.max(leftDepth, rightDepth) +  1;
     }
 
+    // 中序遍历的中间节点
+    TreeNode middleNode;
+    int miniVal = Integer.MAX_VALUE;
+    // 搜索二叉树的中序遍历的节点值会是一个升序数组，
+    // 所以只需要比较相邻的两个节点差值并比较就可以知道最小值
+    public int getMinimumDifference(TreeNode root) {
+        middleOrder(root);
+        return miniVal;
+    }
+    /**
+     * 查找搜索二叉树两个节点的最小差值
+     * @param root
+     */
+    public void middleOrder(TreeNode root){
+        if(null == root){
+            return;
+        }
+        middleOrder(root.left);
+        if(middleNode != null){
+            miniVal = Math.min(Math.abs(middleNode.val - root.val), miniVal);
+        }
+        middleNode = root;
+        middleOrder(root.right);
+    }
+
 }
