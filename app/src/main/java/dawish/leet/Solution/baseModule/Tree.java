02c3f43716d2f0d9acbcd384e51c2d8fefe425fd
dawish <742637254@qq.com>
2020-07-04 10:36:07 +0800

    get tree deepin

--- app/src/main/java/dawish/leet/Solution/baseModule/Tree.java
+++ app/src/main/java/dawish/leet/Solution/baseModule/Tree.java
@@ -1,11 +1,55 @@
 package dawish.leet.Solution.baseModule;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
 import dawish.leet.Solution.twoModule.TreeNode;
 
 public class Tree {
+
+    /**
+     * 递归求二叉树深度
+     * @param root
+     * @return
+     */
+    public static int getTreeDeepin(TreeNode root){
+
+        if(root == null){
+            return  0;
+        }
+        int leftDeepin = getTreeDeepin(root.left) + 1;
+        int rightDeepin = getTreeDeepin(root.right) + 1;
+        return leftDeepin >= rightDeepin ? leftDeepin : rightDeepin;
+    }
+
+    /**
+     * 非递归求二叉树深度
+     * @param root
+     * @return
+     */
+    public static int getTreeDeepin2(TreeNode root){
+
+        if(root == null){
+            return 0;
+        }
+        int deepin = 0;
+        ArrayDeque<TreeNode> deque = new ArrayDeque<TreeNode>();
+        deque.add(root);
+
+        if(!deque.isEmpty()){
+            TreeNode treeNode = deque.removeFirst();
+            if(treeNode.left != null){
+                deque.add(treeNode.left);
+            }
+            if(treeNode.right != null){
+                deque.add(treeNode.right);
+            }
+            deepin++;
+        }
+
+        return deepin;
+    }
 
     /**
      * 二叉树之字打印
