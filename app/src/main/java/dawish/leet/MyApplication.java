a09b0df5285634556dd89ef97f6f8a81c0476d19
Dawish <742637254@qq.com>
2021-04-18 18:24:00 +0800

    add hook Instrumentation

--- app/src/main/java/dawish/leet/MyApplication.java
+++ app/src/main/java/dawish/leet/MyApplication.java
@@ -1,17 +1,32 @@
 package dawish.leet;
 
 import android.app.Application;
-import android.content.res.Configuration;
+import android.os.Process;
 
 import dawish.leet.android.MyUncaughtExceptionHandler;
 import dawish.leet.android.hook.InstrumentationHookHelper;
 
+/**
+ * 多进程会让同一个Application启动两次，onCreate方法执行两次
+ *
+ * @author danxingxi
+ */
 public class MyApplication extends Application {
+
+    private static final String TAG = "MyApplication";
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        DLog.D(TAG, "============================");
+        DLog.D(TAG, "MyApplication:" + MyApplication.this);
+        DLog.D(TAG, "pid:" + Process.myPid());
+        DLog.D(TAG, "thread:" + Thread.currentThread());
+
+        // handle exception
         Thread.setDefaultUncaughtExceptionHandler(new MyUncaughtExceptionHandler());
+        // hook
         InstrumentationHookHelper.hook();
     }
 }
